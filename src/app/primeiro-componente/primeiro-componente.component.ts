03d2cfe0b4abca8205962e44199bfd94fdf0b93f
joaojp <joaopaulo-itujp@hotmail.com>
2022-11-01 23:50:27 -0300

    iniciando a aplicação

---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-primeiro-componente',
+    templateUrl: './primeiro-componente.component.html',
+    styleUrls: ['./primeiro-componente.component.css']
+})
+
+export class PrimeiroComponenteComponent {}
